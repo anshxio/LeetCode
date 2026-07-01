e865af8c26682d089b0c384156dde875ffb30aae
Ansh Sharma <rishu11sharma@gmail.com>
2026-07-02 00:52:07 +0530

    Time: 6 ms (69.95%), Space: 45.7 MB (65.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,16 +5,13 @@ class Solution {
         int n = s.length();
         int l=0, r =0;
         while(r < n){
-            if(!map.containsKey(s.charAt(r))){
-                map.put(s.charAt(r),r);
-                maxLength = Math.max(maxLength, r-l+1);
-                r++;
-            }else{
-                int idx = map.get(s.charAt(r));
-                map.clear();
-                l=r=idx+1;
+            if(map.containsKey(s.charAt(r)) && map.get(s.charAt(r)) >= l){
+                l = map.get(s.charAt(r)) + 1;
             }
-        } 
+            map.put(s.charAt(r),r);
+            maxLength = Math.max(maxLength, r-l+1);
+            r++;
+        }
         return maxLength;
     }
 }
