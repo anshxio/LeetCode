ca31c9a997c1dc52a0c20a21a9b8b7a10d0ba78e
Ansh Sharma <rishu11sharma@gmail.com>
2026-07-21 16:48:30 +0530

    Time: 5 ms (35.13%), Space: 45.2 MB (85.37%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,18 +1,20 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        // code for tabulation
+        // code for tabulation space optimization
         int n = triangle.size();
-        int [][] dp = new int[n][n];
+        int [] front = new int[n];
         for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle.get(n-1).get(j);
+            front[j] = triangle.get(n-1).get(j);
         }
         for(int i= n-2; i>=0; i--){
+            int [] curr = new int[n];
             for(int j=i; j>=0; j--){
-                int up = triangle.get(i).get(j) + dp[i+1][j];
-                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
-                dp[i][j] = Math.min(up,diagonal);
+                int up = triangle.get(i).get(j) + front[j];
+                int diagonal = triangle.get(i).get(j) + front[j+1];
+                curr[j] = Math.min(up,diagonal);
             }
+            front = curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 }
