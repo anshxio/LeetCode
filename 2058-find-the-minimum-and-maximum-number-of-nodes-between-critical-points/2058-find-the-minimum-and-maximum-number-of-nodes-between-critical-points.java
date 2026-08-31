154811ea0f1f2e9df9fffda1dbac8ae70c425f15
Ansh Sharma <rishu11sharma@gmail.com>
2026-08-31 21:01:47 +0530

    Time: 5 ms (40.73%), Space: 105.9 MB (85.47%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,47 +10,40 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-
         ListNode prev = head;
-        ListNode curr = head.next;
-
+        ListNode temp = head.next;
         int index = 1;
-
-        int first = -1;
-        int prevCritical = -1;
-        int last = -1;
+        int firstPoint = -1;
+        int criticalPoint = -1;
+        int lastPoint = -1;
 
         int minDistance = Integer.MAX_VALUE;
 
-        while (curr.next != null) {
+        while(temp.next != null){
+            boolean isMax = prev.val < temp.val && temp.val > temp.next.val;
+            boolean isMin = prev.val > temp.val && temp.val < temp.next.val;
 
-            boolean isCritical = (curr.val > prev.val && curr.val > curr.next.val) ||
-                    (curr.val < prev.val && curr.val < curr.next.val);
+            if(isMax || isMin){
 
-            if (isCritical) {
-                if (first == -1) {
-                    first = index;
-                } else {
-                    minDistance = Math.min(
-                            minDistance,
-                            index - prevCritical);
+                if(firstPoint == -1){
+                    firstPoint = index;
+                    criticalPoint = index;
+                }else{
+                    minDistance = Math.min(minDistance, index - criticalPoint);
+                    criticalPoint = index;
+                    lastPoint = index;
                 }
-
-                prevCritical = index;
-                last = index;
             }
-
-            prev = curr;
-            curr = curr.next;
+            prev = temp;
+            temp = temp.next;
             index++;
         }
-        if (first == last) {
-            return new int[] {-1, -1};
+        if(lastPoint == -1){
+            return new int[]{-1,-1};
         }
 
-        return new int[] {
-                minDistance,
-                last - first
-        };
+        int maxDistance = lastPoint - firstPoint;
+
+        return new int[]{minDistance, maxDistance};
     }
 }
